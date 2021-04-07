09b8e3549c404aad952df9ec154d409980445315
Paul Morales <paulginga@hotmail.com>
2021-04-07 16:14:11 -0500

    aligned rigt side of nav

--- front-end-skills-test/src/App.js
+++ front-end-skills-test/src/App.js
@@ -1,5 +1,7 @@
 import mpLogo from './images/mp_logo-removebg-preview.png'
 import social from './images/socialMedia.png';
+import search from './images/searchCircle.png';
+import searchGlass from './images/carbon_search.png';
 import './App.css';
 
 function App() {
@@ -7,15 +9,17 @@ function App() {
     <div className='App'> 
 
     <nav>
+      
       <ul>
-        <li><a href='#'><img className='social' src={social} alt='social media'/></a></li>
-        <li> Connect Wallet</li>
-        <li> Service</li>
-        <li> Events</li>
-        <li> Learn</li>
-        <li> Join</li>
-        <li> Search</li>
+        <li className='service'> Service</li>
+        <li className='events'> Events</li>
+        <li className='learn'> Learn</li>
+        <li className='join'> Join</li>
       </ul>
+      <li> Connect Wallet</li>
+      <a href='#'><img className='searchCircle' src={search} alt='search'/></a>
+      <a href='#'><img className='search' src={searchGlass} alt='search'/></a>
+      <a href='#'><img className='social' src={social} alt='social media'/></a>
     </nav>
     <img src={mpLogo} alt='mp logo'/>
     </div>
