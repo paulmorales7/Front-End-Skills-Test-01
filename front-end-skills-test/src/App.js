57fae2837664313cffeb12bc550ec3269fe5ba96
Paul Morales <paulginga@hotmail.com>
2021-04-07 15:26:05 -0500

    initial commit, added bg color, nav elements, and logo

---
+++ front-end-skills-test/src/App.js
@@ -0,0 +1,27 @@
+import mpLogo from './images/mp_logo-removebg-preview.png'
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'> 
+
+    <nav>
+      <ul>
+        <li> tw</li>
+        <li> fb</li>
+        <li> in</li>
+        <li> ig</li>
+        <li> Connect Wallet</li>
+        <li> Service</li>
+        <li> Events</li>
+        <li> Learn</li>
+        <li> Join</li>
+        <li> Search</li>
+      </ul>
+    </nav>
+    <img src={mpLogo} alt='mp logo'/>
+    </div>
+  )
+}
+
+export default App;
