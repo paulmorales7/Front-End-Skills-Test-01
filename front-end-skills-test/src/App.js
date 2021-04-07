dd9e61a6a5316b7649131a2e30ba92e5c1cc0bfd
Paul Morales <paulginga@hotmail.com>
2021-04-07 16:36:09 -0500

    completed nav with all elements, next step is to align logo

--- front-end-skills-test/src/App.js
+++ front-end-skills-test/src/App.js
@@ -2,6 +2,7 @@ import mpLogo from './images/mp_logo-removebg-preview.png'
 import social from './images/socialMedia.png';
 import search from './images/searchCircle.png';
 import searchGlass from './images/carbon_search.png';
+import connect from './images/connectW.png';
 import './App.css';
 
 function App() {
@@ -16,7 +17,9 @@ function App() {
         <li className='learn'> Learn</li>
         <li className='join'> Join</li>
       </ul>
-      <li> Connect Wallet</li>
+      <div>
+         <img className='wallet' src={connect} alt='connect wallet'/>
+         </div>
       <a href='#'><img className='searchCircle' src={search} alt='search'/></a>
       <a href='#'><img className='search' src={searchGlass} alt='search'/></a>
       <a href='#'><img className='social' src={social} alt='social media'/></a>
