9a0552b91cfd6fbeba0a8d794fc3fb4ea3a9898f
Paul Morales <paulginga@hotmail.com>
2021-04-07 18:21:11 -0500

    added all elements into rectangle

--- front-end-skills-test/src/App.js
+++ front-end-skills-test/src/App.js
@@ -7,6 +7,8 @@ import connect from './images/connectW.png';
 import butterfly from './images/butterfly.png';
 import world from './images/world.png';
 import filler from './images/filler.png';
+import smallFiller from './images/smallFiller.png';
+import group from './images/Group.png';
 import './App.css';
 
 function App() {
@@ -35,7 +37,11 @@ function App() {
     <div className='rectangle'>
     <img className='butterfly' src={butterfly} alt='butterfly' />
     <img className='filler' src={filler} alt='filler' />
+    <img className='smallFiller' src={smallFiller} alt='small-filler' />
     <img className='world' src={world} alt='world' />
+    <p className='rectP'>Learn high in <br/> Demand IT skills & get crypto</p>
+    <p className='smallText'>powered by KoinStreet</p>
+    <img className='group' src={group} alt='group' />
     </div>
     <img className='logo' src={mpLogo} alt='mp logo'/>
     <img className='logoShape' src={mpLogoShape} alt='mp logo shape'/>
