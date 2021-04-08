01c6595131f814aa6ea6d44afeda765198882d99
Paul Morales <paulginga@hotmail.com>
2021-04-08 15:21:27 -0500

    stlying course cards

--- front-end-skills-test/src/App.js
+++ front-end-skills-test/src/App.js
@@ -1,18 +1,48 @@
 import Courses from './components/courses/courses';
 import Title from './components/title/title';
 import Nav from './components/nav/nav';
+import Wrapper from './components/wrapper/wrapper';
+import course from './courses.json';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
+class App extends Component {
+
+state = {
+  course
+};
+
+  render(){
+    
   return (
     <div className='App'> 
    <Nav />
   <Title />
-<Courses />
-    </div>
 
+<Wrapper>
+  {this.state.course.map(cor => {
+    console.log(cor.image)
+    return(
+    
+    <Courses 
+key={cor.id}
+earn={cor.earn}
+title={cor.title}
+para={cor.para}
+learn={cor.learn}
+modules={cor.modules}
+completed={cor.completed}
+/>
+
+
+
+    )
+  })}
+</Wrapper>
+
+</div>
 
   )
-}
+}}
 
 export default App;
--- front-end-skills-test/src/components/courses/courses.js
+++ front-end-skills-test/src/components/courses/courses.js
@@ -1,13 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './courses.css';
+
+// import border from '../../images/border.png';
 
 function courses(props) {
 
 return (
-    <div >
+    <div className='entire'>
     <h1 className='title'>My Courses</h1>
   
   <div className='card'>
+
+<div className='content'>
+<p>{props.earn}</p>
+<h2>{props.title}</h2>
+<p>{props.para}</p>
+<button>{props.learn}</button>
+<h3>{props.modules}</h3>
+<p>{props.completed}</p>
+</div>
 
   </div>
 
